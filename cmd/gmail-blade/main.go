f7746b1ba6fd7d259e3e85eafa86c2878168efb5
Joe <jc@unknwon.io>
2026-09-23 15:41:26 -0400

    Apply suggestion from @unknwon

--- cmd/gmail-blade/main.go
+++ cmd/gmail-blade/main.go
@@ -505,7 +505,6 @@ func processMessage(logger Logger, ctx context.Context, dryRun bool, config *con
 			if err != nil {
 				return errors.Wrap(err, "process GitHub review action")
 			}
-			// Not approved: skip remaining actions like the "Auto-approved" label.
 			if !approved {
 				logger.Debug("Pull request not approved, skip remaining actions", "uid", msg.UID)
 				break
